a3ccda8b36e931c1999aadd0393f8cdd4b537f50
sanjaydambal <sanjaydambal@gmail.com>
2023-10-13 15:51:54 +0530

    export modules

--- add.js
+++ add.js
@@ -1,5 +1,4 @@
 const add = (x,y)=>{
     return x+y
 }
-const sum = add(5,4);
-console.log(sum);
+module.exports = add;
--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-require("./add")
+const add = require("./add")
 console.log("hello lakshmikant i have started learning node-Js");
+const sum = add(5,4);
+const sum2 = add(8,3);
+console.log(sum);
+console.log(sum2);
