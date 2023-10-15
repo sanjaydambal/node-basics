7d6f468e279aff1997ab3691dee50c1382bf9828
sanjaydambal <sanjaydambal@gmail.com>
2023-10-15 13:20:37 +0530

    iife

---
+++ iife.js
@@ -0,0 +1,8 @@
+(function(message){
+const superhero = "Superman"
+console.log(message,superhero);
+})("hey");
+(function(message){
+    const superhero = "Batman"
+    console.log(message,superhero);
+    })("hello");
